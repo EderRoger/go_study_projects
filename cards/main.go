3a98ae1cbd137db6a52ade10bc83c4b16a450ef4
Eder Roger <eder.roger.souza@gmail.com>
2018-06-04 20:10:47 -0300

    feat: add a new simple odd even test

--- cards/main.go
+++ cards/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// cards := newDeck()
 
@@ -12,7 +16,17 @@ func main() {
 	//cards := newDeckFromFile("cards_game")
 	//cards.print()
 
-	cards := newDeck()
-	cards.shufle()
-	cards.print()
+	// cards := newDeck()
+	// cards.shufle()
+	// cards.print()
+
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, number := range numbers {
+		if number%2 == 0 {
+			fmt.Println("is even", number)
+		} else {
+			fmt.Println("is odd", number)
+		}
+	}
 }

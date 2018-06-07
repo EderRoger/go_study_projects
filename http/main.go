97043b565819ad33e6ee036a6822d00b5efacb9a
Eder Roger <eder.roger.souza@gmail.com>
2018-06-07 07:51:49 -0300

    feat: add new customized method to print the html response body

--- http/main.go
+++ http/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 
@@ -18,6 +20,16 @@ func main() {
 	// byteSlice := make([]byte, 99999)
 	// resp.Body.Read(byteSlice)
 	// fmt.Println(string(byteSlice))
-	io.Copy(os.Stdout, resp.Body) //advanced form to print bytes
 
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body) //advanced form to print bytes
+
+}
+
+func (logWriter) Write(byteSlice []byte) (int, error) {
+	fmt.Println(string(byteSlice))
+	fmt.Println("Just wrote this many bytes:", len(byteSlice))
+
+	return len(byteSlice), nil
 }

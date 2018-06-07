51a02d7a9fa7385f19c538b036e13615118602a8
Eder Roger <eder.roger.souza@gmail.com>
2018-06-07 08:16:12 -0300

    feat: add a new excercise to practice some code about interfaces

---
+++ shape/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+type square struct {
+	sideLength float64
+}
+
+func main() {
+	s := square{sideLength: 2.0}
+	t := triangle{base: 2.0, height: 1.0}
+
+	prinArea(s)
+	prinArea(t)
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func prinArea(s shape) {
+	fmt.Println(s.getArea())
+}

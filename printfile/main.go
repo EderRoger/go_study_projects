ca75e21cdcdebe74e68d92d36ac86b850c2fb04d
Eder Roger <eder.roger.souza@gmail.com>
2018-06-09 08:09:13 -0300

    feat: add method to open a file from args, and check error messages

--- printfile/main.go
+++ printfile/main.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Args)
+
+	if len(os.Args) == 1 {
+		fmt.Println("File name expected as a parameter Example: \"go run main.go filename\"")
+		os.Exit(1)
+	}
+
+	fileName := os.Args[1]
+
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		fmt.Println("Error to open file:", fileName, "error", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, file)
 }

10ef920174f2cdaffe7bf24b5ab6517b4dbdc9fd
Eder Roger <eder.roger.souza@gmail.com>
2019-03-05 09:21:27 -0300

    feat: change code to use a smart way to defining an a variable

--- phase_2/cards/main.go
+++ phase_2/cards/main.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Aces of Spades"
+	// var card string = "Aces of Spades"
+	card := "Aces of Spades" // the same effect
 	fmt.Println(card)
 }

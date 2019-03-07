79204b3d372914d1847351c2aa551ef5f931f799
Eder Roger <eder.roger.souza@gmail.com>
2019-03-07 07:38:31 -0300

    feat: adding a loop to check the data

--- phase_2/cards/main.go
+++ phase_2/cards/main.go
@@ -6,7 +6,10 @@ func main() {
 	// var card string = "Aces of Spades"
 	cards := []string{newCard(), newCard()}
 	cards = append(cards, "The joke")
-	fmt.Println(cards)
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {

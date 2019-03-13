35946b7e17147b877a38bbdac218f2b09b9b71e3
Eder Roger <eder.roger.souza@gmail.com>
2019-03-13 09:51:07 -0300

    feat: add a saveToFile method using ioutil package

--- phase_2/cards/main.go
+++ phase_2/cards/main.go
@@ -3,13 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// // var card string = "Aces of Spades"
-	// cards := newDeck()
-	// hands, remainingDeck := deal(cards, 5)
-
-	// hands.print()
-	// fmt.Println("--------------------------------------------------------")
-	// remainingDeck.print()
 	cards := newDeck()
 	fmt.Println(cards.toString())
 }

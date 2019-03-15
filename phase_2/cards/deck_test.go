488550040d48ed4b6c8cd5c8b90a11440624a59a
Eder Roger <eder.roger.souza@gmail.com>
2019-03-15 09:18:44 -0300

    test: add more tests checking the first and last cards on deck

--- phase_2/cards/deck_test.go
+++ phase_2/cards/deck_test.go
@@ -8,4 +8,14 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expected deck lenght of 20, but got %v", len(d))
 	}
+
+	firstCard := d[0]
+	if firstCard != "Ace of Spades" {
+		t.Errorf("Exepected first card of Ace of Spades, but got %v", firstCard)
+	}
+
+	lastCard := d[len(d)-1]
+	if lastCard != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", lastCard)
+	}
 }

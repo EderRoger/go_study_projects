6c1cf6665d74ec9ef97d026a15e3ca27a8075ce5
Eder Roger <eder.roger.souza@gmail.com>
2019-03-19 09:46:27 -0300

    feat: new struct for contactInfo

--- phase_2/structs/main.go
+++ phase_2/structs/main.go
@@ -2,16 +2,29 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	var p person
 	p.firstName = "Alex"
 
-	eder := person{firstName: "Eder Roger", lastName: "de Souza"}
+	eder := person{
+		firstName: "Eder Roger",
+		lastName:  "de Souza",
+		contact: contactInfo{
+			email:   "eder.r@gmail.com",
+			zipCode: 909374937,
+		},
+	}
 
 	fmt.Println(eder)
 	fmt.Printf("%+v", p)

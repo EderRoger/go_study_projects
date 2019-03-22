da892371b078159ee6f5d66b0e352caa41c71e83
Eder Roger <eder.roger.souza@gmail.com>
2019-03-22 08:07:06 -0300

    feat: new approach with interfaces

---
+++ phase_2/interfaces/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hello there!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}

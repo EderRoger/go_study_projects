ec12922362d2ec08f41d2ce6bcb60e171b9cd32f
Eder Roger <eder.roger.souza@gmail.com>
2019-03-25 09:23:55 -0300

    feat: adding a print method to each routine using channel

--- phase_2/channels/main.go
+++ phase_2/channels/main.go
@@ -15,17 +15,25 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		chechLink(link)
+		go chechLink(link, c)
+	}
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func chechLink(link string) {
+func chechLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, " might be down")
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, " is up")
+	c <- "Yep is up"
 }

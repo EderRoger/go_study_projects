612ed5f1e144d43b037e58e32cb4ab545f0483f4
Eder Roger <eder.roger.souza@gmail.com>
2019-03-25 07:06:11 -0300

    feat: adding a first behavior, checking links ordered

--- phase_2/channels/main.go
+++ phase_2/channels/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://goland.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		chechLink(link)
+	}
+}
+
+func chechLink(link string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, " might be down")
+		return
+	}
+
+	fmt.Println(link, " is up")
 }

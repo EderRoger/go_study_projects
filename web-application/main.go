0b217acc416cefbfcc247c893938f70493f84f45
Eder Roger <eder.roger.souza@gmail.com>
2019-04-02 08:04:25 -0300

    feat: goto examples

--- web-application/main.go
+++ web-application/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	s := "hello"
@@ -9,4 +12,50 @@ func main() {
 	s2 := string(c)
 
 	fmt.Println(s2)
+
+	// import "fmt"
+	// import "os"
+
+	// const i = 100
+	// const pi = 3.1415
+	// const prefix = "Go_"
+
+	// var i int
+	// var pi float32
+	// var prefix string
+
+	// Group form
+
+	// import (
+	// 	"fmt"
+	// 	"os"
+	// )
+
+	// const (
+	// 	i = 100
+	// 	pi = 3.1415
+	// 	prefix = "Go_"
+	// )
+
+	// var (
+	// 	i int
+	// 	pi float32
+	// 	prefix string
+	// )
+	goTO()
+}
+
+func goTO() {
+	i := 0
+
+Here:
+	fmt.Println(i)
+	i++
+	if i == 100 {
+		goto Out
+	}
+
+	goto Here
+Out:
+	os.Exit(1)
 }

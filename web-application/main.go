22f073b567a546487baad089213a052a7b0bfc13
Eder Roger <eder.roger.souza@gmail.com>
2019-04-09 07:46:55 -0300

    feat: adding examples for struct

--- web-application/main.go
+++ web-application/main.go
@@ -5,13 +5,61 @@ import (
 	"os"
 )
 
-func main() {
-	s := "hello"
-	c := []byte(s)
-	c[0] = 's'
-	s2 := string(c)
+type isInt func(int) bool
 
-	fmt.Println(s2)
+func isOdd(number int) bool {
+	return number%2 != 0
+}
+
+func isEven(number int) bool {
+	return number%2 == 0
+}
+
+func filter(slice []int, f isInt) []int {
+	var result []int
+	for _, value := range slice {
+		if f(value) {
+			result = append(result, value)
+		}
+	}
+	return result
+}
+
+var slice = []int{1, 2, 3, 4, 5, 6, 7}
+
+// structs
+
+type Skills []string
+
+type Human struct {
+	name   string
+	age    int
+	weight int
+}
+
+type Student struct {
+	Human
+	Skills
+	int
+	specialty string
+}
+
+func main() {
+	skills := []string{"fly", "strength"}
+	eder := Student{Human: Human{name: "Eder", age: 34, weight: 78}, Skills: skills, specialty: "IT"}
+	fmt.Println(eder)
+	// odd := filter(slice, isOdd)
+	// even := filter(slice, isEven)
+
+	// fmt.Println("Odd: ", odd)
+	// fmt.Println("Even: ", even)
+
+	// s := "hello"
+	// c := []byte(s)
+	// c[0] = 's'
+	// s2 := string(c)
+
+	// fmt.Println(s2)
 
 	// import "fmt"
 	// import "os"
@@ -42,7 +90,7 @@ func main() {
 	// 	pi float32
 	// 	prefix string
 	// )
-	goTO()
+	// goTO()
 }
 
 func goTO() {

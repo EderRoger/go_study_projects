6d22149bfa0e364c4c0c01fb8c973bc1aac1eb03
Eder Roger <eder.roger.souza@gmail.com>
2019-04-09 08:02:03 -0300

    feat: using methods(receivers) to define diferent behaviors

--- web-application/object-oriented/main.go
+++ web-application/object-oriented/main.go
@@ -1,20 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Rectangle struct {
 	width, height float64
 }
 
-func area(r Rectangle) float64 {
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) Area() float64 {
 	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return c.radius * c.radius * math.Pi
 }
 
 func main() {
 	r1 := Rectangle{10, 40}
 	r2 := Rectangle{20, 10}
+	c1 := Circle{30}
 
-	fmt.Println("Area 1 is: ", area(r1))
-	fmt.Println("Area 2 is: ", area(r2))
+	fmt.Println("Area 1 is: ", r1.Area())
+	fmt.Println("Area 2 is: ", r2.Area())
+	fmt.Println("Circle 1 is: ", c1.Area())
 
 }
